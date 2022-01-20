f6aa738deeaaf04c9bd33a88a9963af66826adb4
muirurii <pnkanuna@gmail.com>
2022-01-20 09:25:13 +0300

    completed aboutpage

---
+++ src/components/pages/About/Term.jsx
@@ -0,0 +1,16 @@
+const Term = ({text,number}) => {
+    return (
+        <li data-number={number}>
+            <h2>{text}</h2>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat atque 
+                doloremque accusamus magni numquam nobis quia iure explicabo nostrum 
+                tempora? Veniam, in non, 
+                ipsum labore officiis praesentium obcaecati deleniti 
+                dicta quos dolorum, deserunt consequatur officia!
+            </p>
+        </li>
+    )
+}
+
+export default Term;
